1e06e6b83713caa84de460c4baade8cda17a791c
fatimeh <43520497+fatimeh@users.noreply.github.com>
2021-03-30 19:37:02 -0400

    added header

---
+++ main.py
@@ -0,0 +1,14 @@
+"""CSC111 Winter 2021 Final Project
+
+Copyright and Usage Information
+===============================
+
+This file is provided solely for the personal and private use of students
+taking CSC111 at the University of Toronto St. George campus. All forms of
+distribution of this code, whether as given or with any changes, are
+expressly prohibited.
+
+This file is Copyright (c) 2021 Fatimeh Hassan, Shilin Zhang,
+Dorsa Molaverdikhani, and Nimit Bhanshali.
+"""
+
