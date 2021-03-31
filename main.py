61366ee755260ccb885476291a7b473285330351
dorsa1380 <81657846+dorsa1380@users.noreply.github.com>
2021-03-31 07:42:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ expressly prohibited.
 This file is Copyright (c) 2021 Fatimeh Hassan, Shilin Zhang,
 Dorsa Molaverdikhani, and Nimit Bhanshali.
 """
+from __future__ import annotations
 from dataclasses import dataclass
-from typing import Tuple
+from typing import Tuple, Dict, Set
 
 # Reading .csv files
 
@@ -52,5 +53,34 @@ class Movie:
 
 
 # Vertex Class
+class _Vertex:
+    """
+    A vertex in the graph.
+    
+    Each vertex item is an instance of a Movie class.
+    
+    The neighbours is a dictionary which maps the _Vertex object to the set of the traits
+    which this Vertex and its neighbour have in common.
+    
+    A Vertex is in the neighbours of this Vertex if it has at least one trait
+    in common with this Vertex.
+
+    Instance Attributes:
+        - item: The data stored in this vertex, representing a movie.
+        - neighbours: The vertices that are adjacent to this vertex.
+
+    Representation Invariants:
+        - self not in self.neighbours
+        - all(self in u.neighbours for u in self.neighbours)
+        - all(self.neighbours[v] != set() for v in self.neighbours)
+    """
+    
+    item: Movie
+    neighbours: Dict[_Vertex, Set[str]]
+
+    def __init__(self, item: Movie, neighbours: Dict[_Vertex, Set[str]]) -> None:
+        """Initialize a new vertex with the given item and neighbours."""
+        self.item = item
+        self.neighbours = neighbours
 
 # Graph Class
