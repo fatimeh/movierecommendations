b64459e986bb27b9e11f2c30efc7a6247d243bc8
fatimeh <43520497+fatimeh@users.noreply.github.com>
2021-03-30 20:34:05 -0400

    created movie class

--- main.py
+++ main.py
@@ -11,9 +11,48 @@ expressly prohibited.
 This file is Copyright (c) 2021 Fatimeh Hassan, Shilin Zhang,
 Dorsa Molaverdikhani, and Nimit Bhanshali.
 """
+from dataclasses import dataclass
+from typing import Tuple
+
 # Reading .csv files
 
+
 # Movie Object Class
+@dataclass
+class Movie:
+    """
+    A data class representing a movie.
+
+    Instance Attributes:
+    - title: the title of the movie
+    - release_year: the year the movie was released
+    - genre: the genre(s) of the movie
+    - duration: the length of the movie (in minutes)
+    - country: the name of the country/countries the movie was originally released in
+    - language: the language the movie was written in
+    - director: the name of the person that directed the movie
+
+    Representation Invariants:
+
+    """
+    title: str
+    release_year: int
+    genre: Tuple[str]
+    duration: int
+    country: Tuple[str]
+    language: str
+    director: str
+
+    def __init__(self, title: str, release_year: int, genre: Tuple[str], duration: int,
+                 country: Tuple[str], language: str, director: str):
+        self.title = title
+        self.release_year = release_year
+        self.genre = genre
+        self.duration = duration
+        self.country = country
+        self.language = language
+        self.director = director
+
 
 # Vertex Class
 
