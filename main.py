db427fdad0c7e8f03789aa18a2da6c1fd2d6f3b6
fatimeh <43520497+fatimeh@users.noreply.github.com>
2021-04-11 18:25:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -18,9 +18,7 @@ This file is Copyright (c) 2021 Fatimeh Hassan, Shilin Zhang,
 Dorsa Molaverdikhani, and Nimit Bhanshali.
 """
 from __future__ import annotations
-from dataclasses import dataclass
-from typing import Set, List, Any
-import pandas as pd
-import tkinter as tk
+from entities import Movie, _MovieVertex, MovieGraph
+from visualization import main_runner
 
 #Main runner function
