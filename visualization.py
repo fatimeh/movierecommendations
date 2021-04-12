0d6c671ae24b903f2f9505571df66a01664fa48f
shilinzhang-cs <75181480+shilinzhang-cs@users.noreply.github.com>
2021-04-12 14:46:40 -0400

    docstring update on visualization
    docstring update on visualization

--- visualization.py
+++ visualization.py
@@ -33,34 +33,38 @@ LANGUAGES = ['Quechua', 'Gujarati', 'Kabyle', 'Mari', 'Filipino', 'Mongolian', '
 
 def runner_questions() -> Dict[str, Any]:
     """
-    Returns a dictionary where the keys are the options for rankings and the values are the
-    user input.
+    Returns a dictionary where the keys are the 4 attributes that we asked the user to choose
+    and the values are the user inputs for the 4 questions that we asked.
     """
-    options = {}
+    answers_so_far = {}
     window = tk.Tk()
     window.geometry("600x600")
-    create_genres_listbox(window, options)
-    create_duration_listbox(window, options)
-    create_year_listbox(window, options)
-    create_language_listbox(window, options)
+    create_genres_listbox(window, answers_so_far)
+    create_duration_listbox(window, answers_so_far)
+    create_year_listbox(window, answers_so_far)
+    create_language_listbox(window, answers_so_far)
 
     window.mainloop()
-    return options
+    return answers_so_far
 
 
 def create_decade_options(start_year: int, end_year: int) -> tuple:
-    """Return a tuple."""
-    decades = []
+    """Return a tuple of list where the first list contains all the range that we asked the user
+    to choose as texts and the second list contains the same range except that each item are a tuple
+    of integers where the first integer is the starting year and the second number
+    is the ending year."""
+    decades_string = []
     decades_tuple = []
     for i in range(start_year, end_year, 10):
-        decades.append(f'{i}' + '-' + f'{i + 10}')
+        decades_string.append(f'{i}' + '-' + f'{i + 10}')
         decades_tuple.append((i, i + 10))
 
-    return (decades, decades_tuple)
+    return (decades_string, decades_tuple)
 
 
-def create_genres_listbox(window: tk.Tk, options: dict) -> None:
-    """Create listbox"""
+def create_genres_listbox(window: tk.Tk, user_answer: dict) -> None:
+    """Create the listbox that allows user to choose their genres preference
+     and a button to submit their input"""
     genre_frame = tk.LabelFrame(window, text="What genre do you prefer?", padx=15, pady=15)
     genre_frame.grid(row=0, column=0)
     genre_listbox = tk.Listbox(genre_frame, height=5, selectmode='SINGLE')
@@ -68,19 +72,21 @@ def create_genres_listbox(window: tk.Tk, options: dict) -> None:
         genre_listbox.insert(i + 1, GENRES[i])
 
     def submit() -> None:
-        """Collect user input and destroy the frame."""
-        options['genres'] = GENRES[genre_listbox.curselection()[0]]
+        """Collect whatever user choose for their preference for genres and close the window if
+        user answers all the question on the window."""
+        user_answer['genres'] = GENRES[genre_listbox.curselection()[0]]
         genre_frame.destroy()
-        if len(options) == 4:
+        if len(user_answer) == 4:
             window.destroy()
 
-    button = tk.Button(genre_frame, text='Submit', command=submit)
-    button.pack(side='bottom')
+    submit_button = tk.Button(genre_frame, text='Submit', command=submit)
+    submit_button.pack(side='bottom')
     genre_listbox.pack()
 
 
-def create_duration_listbox(window: tk.Tk, options: dict) -> None:
-    """Create listbox2."""
+def create_duration_listbox(window: tk.Tk, user_answer: dict) -> None:
+    """Create the listbox that allows user to choose their movie duration preference
+         and a button to submit their input"""
     duration_frame = tk.LabelFrame(window, text="What duration do you want?", padx=15, pady=15)
     duration_frame.grid(row=0, column=1)
     duration_listbox = tk.Listbox(duration_frame, height=5, selectmode='SINGLE')
@@ -89,41 +95,45 @@ def create_duration_listbox(window: tk.Tk, options: dict) -> None:
     duration_listbox.insert(3, "Long (>180 min)")
 
     def submit() -> None:
-        """Collect user input."""
-        options['duration'] = DURATIONS[duration_listbox.curselection()[0]]
+        """Collect whatever user choose for their preference for movie duration and close the
+        window if user answers all the question on the window."""
+        user_answer['duration'] = DURATIONS[duration_listbox.curselection()[0]]
         duration_frame.destroy()
-        if len(options) == 4:
+        if len(user_answer) == 4:
             window.destroy()
 
-    button = tk.Button(duration_frame, text='Submit', command=submit)
-    button.pack(side='bottom')
+    submit_button = tk.Button(duration_frame, text='Submit', command=submit)
+    submit_button.pack(side='bottom')
     duration_listbox.pack()
 
 
-def create_year_listbox(window: tk.Tk, options: dict) -> None:
-    """Create listbox2."""
+def create_year_listbox(window: tk.Tk, user_answer: dict) -> None:
+    """Create the listbox that allows user to choose their release year preference
+     and a button to submit their input"""
     year_frame = tk.LabelFrame(window, text="Which decade do you prefer?", padx=15, pady=15)
     year_frame.grid(row=1, column=0)
-    decades = create_decade_options(1960, 2020)[0]
+    decades_string = create_decade_options(1960, 2020)[0]
     decades_tuples = create_decade_options(1960, 2020)[1]
-    year_listbox = tk.Listbox(year_frame, height=len(decades), selectmode='SINGLE')
-    for i in range(1, len(decades) + 1):
-        year_listbox.insert(i, decades[i - 1])
+    year_listbox = tk.Listbox(year_frame, height=len(decades_string), selectmode='SINGLE')
+    for i in range(1, len(decades_string) + 1):
+        year_listbox.insert(i, decades_string[i - 1])
 
     def submit() -> None:
-        """Collect user input."""
-        options['release_year'] = decades_tuples[year_listbox.curselection()[0]]
+        """Collect whatever user choose for their preference for release year and close
+        the window if user answers all the question on the window."""
+        user_answer['release_year'] = decades_tuples[year_listbox.curselection()[0]]
         year_frame.destroy()
-        if len(options) == 4:
+        if len(user_answer) == 4:
             window.destroy()
 
-    button = tk.Button(year_frame, text='Submit', command=submit)
-    button.pack(side='bottom')
+    submit_button = tk.Button(year_frame, text='Submit', command=submit)
+    submit_button.pack(side='bottom')
     year_listbox.pack()
 
 
-def create_language_listbox(window: tk.Tk, options: dict) -> None:
-    """Create listbox4."""
+def create_language_listbox(window: tk.Tk, user_answer: dict) -> None:
+    """Create the listbox that allows user to choose their language preference
+     and a button to submit their input"""
     language_frame = tk.LabelFrame(window, text="Which language do you want?", padx=15, pady=15)
     language_frame.grid(row=1, column=1)
     language_listbox = tk.Listbox(language_frame, height=5, selectmode='SINGLE')
@@ -131,20 +141,24 @@ def create_language_listbox(window: tk.Tk, options: dict) -> None:
         language_listbox.insert(j + 1, LANGUAGES[j])
 
     def submit() -> None:
-        """Collect user input."""
-        options['language'] = LANGUAGES[language_listbox.curselection()[0]]
+        """Collect whatever user choose for their preference for language and close the window if
+        user answers all the question on the window."""
+        user_answer['language'] = LANGUAGES[language_listbox.curselection()[0]]
         language_frame.destroy()
-        if len(options) == 4:
+        if len(user_answer) == 4:
             window.destroy()
 
-    button = tk.Button(language_frame, text='Submit', command=submit)
-    button.pack(side='bottom')
+    submit_button = tk.Button(language_frame, text='Submit', command=submit)
+    submit_button.pack(side='bottom')
     language_listbox.pack()
 
 
 def runner_rankings() -> list:
-    """Return the a list of rankings that the user chooses."""
-    selected = []
+    """Return the a list of rankings that the user chooses.
+    The first item is the attributes that the user rank first which is the most important
+    The second item is the second most important attributes that the user rank and so on.
+    """
+    rank = []
     window = tk.Tk()
     window.geometry("800x800")
     tk.Label(window, text="Ranking").pack()
@@ -155,36 +169,48 @@ def runner_rankings() -> list:
                           "submit").pack()
     tk.Label(window, text="Repeatedly select and click submit in order of importance"
                           " until you select all four").pack()
-    listbox = tk.Listbox(window, height=5, selectmode='SINGLE')
-    listbox.insert(1, 'genre')
-    listbox.insert(2, 'release_year')
-    listbox.insert(3, 'language')
-    listbox.insert(4, 'duration')
+    ranking_listbox = tk.Listbox(window, height=5, selectmode='SINGLE')
+    ranking_listbox.insert(1, 'genre')
+    ranking_listbox.insert(2, 'release_year')
+    ranking_listbox.insert(3, 'language')
+    ranking_listbox.insert(4, 'duration')
 
     def submit() -> None:
         """Collect user selection and delete the option from listbox."""
-        selected.append(RANKINGS[listbox.curselection()[0]])
-        RANKINGS.remove(RANKINGS[listbox.curselection()[0]])
-        listbox.delete(listbox.curselection()[0])
-        if listbox.size() == 0:
+        rank.append(RANKINGS[ranking_listbox.curselection()[0]])
+        RANKINGS.remove(RANKINGS[ranking_listbox.curselection()[0]])
+        ranking_listbox.delete(ranking_listbox.curselection()[0])
+        if ranking_listbox.size() == 0:
             window.destroy()
 
-    btn1 = tk.Button(window, text='Submit', command=submit)
-    btn1.pack(side='bottom')
-    listbox.pack()
+    submit_button = tk.Button(window, text='Submit', command=submit)
+    submit_button.pack(side='bottom')
+    ranking_listbox.pack()
     window.mainloop()
-    return selected
+    return rank
 
 
 def main_runner() -> tuple:
-    """Main runner"""
+    """Display the ranking and the questions window to get the user input and return
+    the output of the 2 methods as a tuple where the first is the ranking output and
+    the second is their answers to the fours question that we ask."""
     rankings = runner_rankings()
     questions = runner_questions()
     return (rankings, questions)
 
 
 def display_recommended_movies(recommended_movies: List) -> None:
-    """Display the recommended movies"""
+    """Display all the recommended movies for the particular user input in order of how much
+    we recommend the movie.
+    The first movie that is displayed on the screen is the most recommended movie and
+    the second movie is the second most recommended and so on.
+
+    The recommended_movies are the list of recommended movies
+
+    Preconditions:
+        - len(recommended_movies) == 10
+
+    """
     window = tk.Tk()
     window.geometry("500x500")
 
